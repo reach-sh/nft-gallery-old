999ff2e9ec7066a7dfad719858d83bf01408938a
Zetsuboii <hmzkrb@gmail.com>
2021-10-22 22:07:08 +0300

    start screen mobile classes

--- src/views/Start.tsx
+++ src/views/Start.tsx
@@ -29,14 +29,14 @@ const Start = () => {
       <img src={Reach} alt="Reach Logo" className="absolute bottom-12 text-center w-16" />
       <div>
         <h1
-          className="text-9xl mb-20 font-bold text-white"
+          className="text-6xl md:text-7xl lg:text-8xl xl:text-9xl mb-20 font-bold text-center text-white"
           style={{ fontFamily: "'Syne', sans-serif" }}
         >
           ga//ery
         </h1>
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-row justify-between mx-4 gap-8">
           <button
-            className="p-4 rounded-md text-white bg-indigo-700 hover:bg-indigo-500"
+            className="p-2 md:p-3 lg:p-3 xl:p-4 rounded-md text-white bg-indigo-700 hover:bg-indigo-500"
             style={{ fontFamily: "'Syne', sans-serif" }}
             onClick={goToLib}
           >
@@ -44,13 +44,13 @@ const Start = () => {
           </button>
 
           <div className="flex rounded-md bg-gray-200">
-            <button onClick={handleGetModal(true)}>
+            <button onClick={handleGetModal(true)} className="flex h-full">
               <span className="material-icons self-center mx-3 opacity-50">play_arrow</span>
             </button>
             <input
               value={appId}
               onChange={handleAppId}
-              className="anaheim h-full rounded-r-md pl-5 text-xl"
+              className="anaheim h-full rounded-r-md pl-2 text-lg xl:text-xl"
               placeholder="Search for an app"
             />
           </div>
